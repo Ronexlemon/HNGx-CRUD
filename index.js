7e00ec4b685f2db837cbdbea04defad12b5060b1
RonexLemon <ronexondimu@gmail.com>
2023-09-14 15:51:50 +0300

    change index

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const Server = require("./server");
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const App =  express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+// Configure bodyparser to handle post requests
+App.use(bodyParser.urlencoded({extended: false}));
+App.use(bodyParser.json());
+const mongoString = process.env.Mongo;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on('error',(error)=>{
+    console.log(error);
+});
+database.once('connected',()=>{
+    console.log("Database connected");
+})
 
-Server();
+App.use(express.json());
+App.use(cors());
+
+App.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+App.use("/api",userRoute);
+App.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
