9336eb4bdccfa763537765a997624009af1834c5
RonexLemon <ronexondimu@gmail.com>
2023-09-14 17:04:44 +0300

    bad idea

--- database/mongo.js
+++ database/mongo.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 
 
     module.exports = async () => {
-      const mongoString = process.env.Mongo;
-      mongoose.connect(mongoString);
+      //const mongoString = process.env.Mongo;
+      mongoose.connect('mongodb+srv://csin45159:csin45159@cluster0.h5qlphb.mongodb.net/?retryWrites=true&w=majority');
       const database = mongoose.connection;
     
       // You can add event listeners to handle connection events here if needed
